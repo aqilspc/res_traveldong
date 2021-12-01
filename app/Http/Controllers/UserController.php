7176a20630fb488b245ddf20a2cc17caf960df87
root <aqil@intermediatech.id>
2021-12-01 10:31:10 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return 'THIS INDEX OF BACKEND OF API TRAVELANDONG.MY.ID';
+    }
+
+    public function login()
+    {
+        
+    }
+}
