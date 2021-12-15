11f2ecee8e4ccc27f5e8427f762376cd4924df60
root <aqil@intermediatech.id>
2021-12-15 16:02:03 +0700

    update user

--- app/Models/Postingan.php
+++ app/Models/Postingan.php
@@ -9,7 +9,7 @@ class Postingan{
 	{
 		$data = DB::table('posts as ps')
 		->join('travels as ts','ts.id','=','ps.id_travel')
-		->select('ps.*','ts.nama_travel')
+		->select('ps.*','ts.nama_travel','ts.logo')
 		->get();
 		return $data;
 
