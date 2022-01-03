1f5d3ae636a2b796b88dfdd9c0622cd629a80eee
Devanada <aqil@intermediatech.id>
2022-01-03 20:40:37 +0700

    testing request

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User{
             $tmp_file_path = "customer/";
             $file->move($tmp_file_path,$tmp_file_name);
             $result = 'customer'.'/'.$tmp_file_name;
-        return $result;
+        return url('customer').'/'.$tmp_file_name;
     }
 
 	public function login($request)
@@ -120,8 +120,8 @@ class User{
 					'created_at'=>Carbon::now()->toDateTimeString()
 				]);
 		if($request->file('customer')!=null){
-			$this->uploadFile($request,'customer');
-			$file_name = 'customer';
+			$sg = $this->uploadFile($request,'customer');
+			$file_name = $sg;
 			$upfoto = $this->oracle()->upFileOracle($file_name);
 			DB::table('customers')->where('id_user',$request->id)->update(['photo'=>$upfoto['message']]);
 		}
