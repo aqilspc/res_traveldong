81f6229e0487d583237cdb51827853e636f97d50
Devanada <aqil@intermediatech.id>
2022-01-04 08:17:44 +0700

    user get by username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
 
     public function getUserByUsername($username)
     {
-        $data = $this->model()->getUserByUsername($id);
+        $data = $this->model()->getUserByUsername($username);
         $hasil = [];
         if($data)
         {
